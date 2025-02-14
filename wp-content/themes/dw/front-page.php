4c6e80312c321be107ce91b0fcf9b86eb3094a37
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-02-14 14:42:55 +0100

    Adding duplicate code into files (header.php and footer.php))

--- wp-content/themes/dw/front-page.php
+++ wp-content/themes/dw/front-page.php
@@ -1,18 +1,5 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_title(' - ', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
-<header>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
-</header>
-<main>
+<?php get_header()?>
+
     <h2>Bienvenu sur mon site&nbsp;!</h2>
     <?php
     //On ouvre 'la Boucle' (The loop), la structure de contrôle de contenu propre de WordPress:
@@ -25,6 +12,5 @@
     endwhile; else: ?>
         <p>Il n’a pas de contenu à afficher.</p>
     <?php endif; ?>
-</main>
-</body>
-</html>
+
+<?php get_footer()?>
--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,19 +1,7 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_title(' - ', false, 'right') . get_bloginfo('name') ?></title>
-</head>
-<body>
-<header>
-    <h1><?= get_bloginfo('name') ?></h1>
-    <p><?= get_bloginfo('description') ?></p>
-</header>
-<main>
-    <?php
+<!-- fonction wordpress qui appelle le fichier header.php-->
+<?php get_header()?>
+
+<?php
     //On ouvre 'la Boucle' (The loop), la structure de contrôle de contenu propre de WordPress:
     if (have_posts()): while (have_posts()): the_post(); ?>
         <h2><?= get_the_title(); ?></h2>
@@ -25,6 +13,6 @@
     endwhile; else: ?>
         <p>Il n’a pas de contenu à afficher.</p>
     <?php endif; ?>
-</main>
-</body>
-</html>
+
+<!-- fonction wordpress qui appelle le fichier footer.php -->
+<?php get_footer()?>
