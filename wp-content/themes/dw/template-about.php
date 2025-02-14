aa5de0b47647fb9ec8377e26c70a1338da7080c2
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-02-14 14:51:10 +0100

    Create Template "À propos"

---
+++ wp-content/themes/dw/template-about.php
@@ -0,0 +1,17 @@
+<?php /* Template Name:Template "À propos"*/ ?>
+<?php get_header()?>
+
+    <h2>À propos de moi&nbsp;!</h2>
+<?php
+    //On ouvre 'la Boucle' (The loop), la structure de contrôle de contenu propre de WordPress:
+    if (have_posts()): while (have_posts()): the_post(); ?>
+        <div >
+            <?= get_the_content();?>
+        </div>
+    <?php
+        // On ferme 'la Boucle' (The loop):
+    endwhile; else: ?>
+        <p>Il n’a pas de contenu à afficher.</p>
+    <?php endif; ?>
+
+<?php get_footer()?>
